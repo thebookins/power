7f18634738604d2340a2a04ea391310ef5a90b05
thebookins <dickens.paul@gmail.com>
2015-11-11 20:52:27 +1100

    updated to work with Python 3.1.0, output every 10s, simulate solar

--- monitor.py
+++ monitor.py
@@ -3,6 +3,9 @@
 """
 	Modified by KieranC to submit pulse count to Open Energy Monitor EmonCMS API
 
+	Modified by thebookins to run with Python 3.1.0, output instantaneous power
+	every 10 seconds, and simulate solar output
+
 	Power Monitor
 	Logs power consumption to an SQLite database, based on the number
 	of pulses of a light on an electricity meter.
@@ -28,19 +31,16 @@
 	SOFTWARE.
 """
 
-import time, os, subprocess, httplib, datetime
-from apscheduler.scheduler import Scheduler
+import time, os, subprocess, httplib,  math
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
 
 # The next 2 lines enable logging for the scheduler. Uncomment for debugging.
 #import logging
 #logging.basicConfig()
 
-pulsecount=0
 power=0
-
-# Start the scheduler
-sched = Scheduler()
-sched.start()
+lasttime=0
 
 
 # This function monitors the output from gpio-irq C app
@@ -55,21 +55,45 @@ def runProcess(exe):
         break
 
 
-# Every minute this function converts the number of pulses over the last minute into a power value and sends it to EmonCMS
-@sched.interval_schedule(minutes=1)
-def SendPulses():
-	global pulsecount
-	global power
-#	print ("Pulses: %i") % pulsecount # Uncomment for debugging.
-	# The next line calculates a power value in watts from the number of pulses, my meter is 1000 pulses per kWh, you'll need to modify this if yours is different.
-	power = pulsecount * 60
-#	print ("Power: %iW") % power # Uncomment for debugging.
-	pulsecount = 0;
-	timenow = time.strftime('%s')
-        url = ("/emoncms/input/post?time=%s&node=1&json={power:%i}&apikey=<insert API key here>") % (timenow, power) # You'll need to put in your API key here from EmonCMS
+# This function converts an interval between pulses (in seconds) to an instantaneous power reading (in Watts)
+# For a meter that outputs 1000 pulses per kWh (modify if different)
+def intervalToPower(interval):
+	return 3600 / interval
+
+# This function sends the instantaneous power to EmonCMS (called every 10 seconds below)
+def SendPulses(
+	global powe
+
+	# Calculate ideal solar output for a 3kW system in Sydney (+10 hours), for simulation purposes in EmonCMS
+	# remove if not needed
+        solarnow = datetime.utcnow() + timedelta(hours=10)
+	seconds_since_midday = (solarnow - solarnow.replace(hour=12, minute=0, second=0, microsecond=0)).total_seconds()
+	theta = (math.pi / 2) * seconds_since_midday / (6 * 3600)
+
+	solar = 3000 * math.cos(theta)
+	solar = max(solar,0)
+
+	# Calculate the power if an impulse had arrived now; if this is less than power, reduce power to this value
+	# as we know the power cannot be greater
+	provisionalPower = intervalToPower((time.time()*1000 - lasttime) / 1000)
+	if(provisionalPower < power):
+	  power = provisionalPower
+
+#	print ("Power: %iW) % power # Uncomment for debugging.
+        url = ("/emoncms/input/post?node=1&json={power:%0.1f,solar:%0.1f}&apikey=<insert API key here>") % (power, solar) # You'll need to put in your API key here from EmonCMS
         connection = httplib.HTTPConnection("localhost")
         connection.request("GET", url)
 
+# Start the scheduler
+sched = BackgroundScheduler()
+sched.add_job(SendPulses, 'interval', seconds=10)
+sched.start()
 
-for line in runProcess(["/usr/local/bin/gpio-irq", "7"]):
-    pulsecount += 1
+lasttime = time.time()*1000
+for line in runProcess(["/usr/local/bin/gpio-irq", "7"]): # GPIO pin 7 on the Pi
+    timenow = time.time()*1000
+    if(timenow >= lasttime + 360): # ignore multiple pulses within a 360 ms window (limits power reported to 10 kW)
+      period_in_seconds = (timenow - lasttime) / 1000
+      power = intervalToPower(period_in_seconds)
+      lasttime = timenow
+
